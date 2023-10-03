4427feec44bfd27a679ab8e2a47ed82c303b91cd
wasyl229 <michal.wasiak@hotmail.com>
2023-10-03 17:25:58 +1030

    Zmienne

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// var myAge = 33;
+
+int myAge = 120;
+int newAge = myAge + 5;
+Console.WriteLine(newAge);
+int myVar = -2;
+uint myVar2 = uint.MaxValue; //uint = bez wartosci ujemnych
+long myVar3 = ulong.MaxValue;
+
+
+
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+decimal myNumber3 = decimal.MaxValue;
