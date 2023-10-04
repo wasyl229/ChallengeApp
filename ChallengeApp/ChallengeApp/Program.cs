d2bbf125fcedd9b57b4ee62fb3f99534937e1f07
wasyl229 <michal.wasiak@hotmail.com>
2023-10-04 16:49:36 +1030

    Zadanie dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,14 +1,103 @@
-﻿// var myAge = 33;
+﻿var yourName = "Ewa";
+var yourSex = "K";
+var yourAge = 33;
 
+if (yourSex == "K" && yourAge < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (yourName == "Ewa" && yourSex == "K" && yourAge == 33) 
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else if (yourAge < 18 && yourSex != "K")
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var myAge = 33;
+//liczby calkowite
 int myAge = 120;
 int newAge = myAge + 5;
-Console.WriteLine(newAge);
+//Console.WriteLine(newAge);
 int myVar = -2;
 uint myVar2 = uint.MaxValue; //uint = bez wartosci ujemnych
-long myVar3 = ulong.MaxValue;
+ulong myVar3 = ulong.MaxValue;
 
 
-
+// liczby zmiennoprzecinkowe
 float myNumber = float.MaxValue;
 double myNumber2 = double.MaxValue;
 decimal myNumber3 = decimal.MaxValue;
+
+// zmienne tekstowe
+string name = "Michal";
+string surname = "Wasiak";
+string result = name + surname + myAge;
+//Console.WriteLine(result);
+char myVar4 = 'c';
+var result2 = name.ToArray();
+
+// zmienne logiczne
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+//Console.WriteLine(isValid);
+*/
+
+/*
+var number1 = 10;
+var number2 = 30;
+
+//  ==
+//  !=
+//  <
+//  >
+//  <=
+//  >=
+
+var name = "Michal";
+var age = 42;
+
+if (age > 50)
+{
+    Console.WriteLine("Jestem Michal lub mam mniej niz 50");
+}
+else if(age > 40)
+{
+    Console.WriteLine("40-50 latek");
+}
+else if(age > 30)
+{
+    Console.WriteLine("30 latek");
+
+}
+else
+{
+    Console.WriteLine("Jestes mlody");
+}
+
+//&&
+//||
+//!
+
+*/
+
